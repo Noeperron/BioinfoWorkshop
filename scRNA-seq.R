a8ab2405278f46ed54bbdba58e98f604084f775b
Noé Perron <107487325+Noeperron@users.noreply.github.com>
2024-08-05 12:34:33 -0400

    Add files via upload

---
+++ scRNA-seq.R
@@ -0,0 +1,93 @@
+# Remove package 'Matrix' and reinstall it
+remove.packages('Matrix')
+install.packages("Matrix", repos="http://R-Forge.R-project.org")
+
+# Install packages
+install.packages('tidyverse')
+install.packages('remotes')
+library(remotes)
+remotes::install_version("Seurat", "4.3.0")
+install.packages('patchwork')
+reticulate::py_install(packages = 'umap-learn')
+
+# Load the packages
+library(dplyr)
+library(Seurat)
+library(patchwork)
+
+# Load the PBMC dataset
+pbmc.data <- Read10X(data.dir = "./filtered_gene_bc_matrices/hg19/")
+
+# Initialize the Seurat object with the raw (non-normalized data)
+pbmc <- CreateSeuratObject(counts = pbmc.data, project = "pbmc3k", min.cells = 3, min.features = 200)
+pbmc
+
+# Let’s examine a few genes in the first thirty cells
+pbmc.data[c("CD3D", "TCL1A", "MS4A1"), 1:30]
+
+# Calculate mitochondrial QC metrics with the PercentageFeatureSet() function. Use the set of all genes starting with MT- as a set of mitochondrial genes
+pbmc[["percent.mt"]] <- PercentageFeatureSet(pbmc, pattern = "^MT-")
+
+# Visualize QC metrics as a violin plot
+VlnPlot(pbmc, features = c("nFeature_RNA", "nCount_RNA", "percent.mt"), ncol = 3)
+
+# Filter cells that have unique feature counts over 2,500 or less than 200, and filter cells that have >5% mitochondrial counts
+pbmc <- subset(pbmc, subset = nFeature_RNA > 200 & nFeature_RNA < 2500 & percent.mt < 5)
+
+# Normalize the data 
+pbmc <- NormalizeData(pbmc, normalization.method = "LogNormalize", scale.factor = 10000)
+
+## We next calculate a subset of features that exhibit high cell-to-cell variation in the dataset (i.e, they are highly expressed in some cells, and lowly expressed in others). 
+
+# Identification of highly variable features (feature selection)
+pbmc <- FindVariableFeatures(pbmc, selection.method = "vst", nfeatures = 2000)
+
+# Identify the 10 most highly variable genes
+top10 <- head(VariableFeatures(pbmc), 10)
+
+# plot variable features with and without labels
+plot1 <- VariableFeaturePlot(pbmc)
+plot2 <- LabelPoints(plot = plot1, points = top10, repel = TRUE)
+plot1 + plot2
+
+# Scaling the data
+all.genes <- rownames(pbmc)
+pbmc <- ScaleData(pbmc, features = all.genes)
+
+# Perform linear dimensional reduction. By default, only the previously determined variable features are used as input, but can be defined using features argument if you wish to choose a different subset.
+pbmc <- RunPCA(pbmc, features = VariableFeatures(object = pbmc))
+DimPlot(pbmc, reduction = "pca")
+
+# ElbowPlot allows for a ranking of principle components based on the percentage of variance explained by each one. 
+ElbowPlot(pbmc)
+
+# Cluster the cells using first 10 PCs
+pbmc <- FindNeighbors(pbmc, dims = 1:10)
+pbmc <- FindClusters(pbmc, resolution = 0.5)
+
+# Run UMAP
+pbmc <- RunUMAP(pbmc, dims = 1:10)
+DimPlot(pbmc, reduction = "umap")
+
+# Finding differentially expressed features (cluster biomarkers)
+
+# find all markers of cluster 2 
+cluster2.markers <- FindMarkers(pbmc, ident.1 = 2) 
+head(cluster2.markers, n = 5)
+
+# find all markers distinguishing cluster 5 from clusters 0 and 3
+cluster5.markers <- FindMarkers(pbmc, ident.1 = 5, ident.2 = c(0, 3))
+head(cluster5.markers, n = 5)
+
+# Find markers for every cluster compared to all remaining cells, report only the positive ones
+pbmc.markers <- FindAllMarkers(pbmc, only.pos = TRUE, min.pct = 0.25, logfc.threshold = 0.25)
+pbmc.markers %>%  group_by(cluster) %>%  slice_max(n = 2, order_by = avg_log2FC)
+
+# Feature marker genes
+FeaturePlot(pbmc, features = c("MS4A1", "GNLY", "CD3E", "CD14", "FCER1A", "FCGR3A", "LYZ", "PPBP","CD8A")) # This is a single-line command.
+
+# Use canonical markers to easily match the unbiased clustering to known cell types
+new.cluster.ids <- c("Naive CD4 T", "CD14+ Mono", "Memory CD4 T", "B", "CD8 T", "FCGR3A+ Mono", "NK", "DC", "Platelet")
+names(new.cluster.ids) <- levels(pbmc)
+pbmc <- RenameIdents(pbmc, new.cluster.ids)
+DimPlot(pbmc, reduction = "umap", label = TRUE, pt.size = 0.5) + NoLegend()
