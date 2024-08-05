5dc4f06467237ac97d332fbb42cdc455a993d487
Noé Perron <107487325+Noeperron@users.noreply.github.com>
2024-08-05 12:42:18 -0400

    Update scRNA-seq.R

--- scRNA-seq.R
+++ scRNA-seq.R
@@ -1,3 +1,5 @@
+# Download the files https://cf.10xgenomics.com/samples/cell/pbmc3k/pbmc3k_filtered_gene_bc_matrices.tar.gz 
+
 # Remove package 'Matrix' and reinstall it
 remove.packages('Matrix')
 install.packages("Matrix", repos="http://R-Forge.R-project.org")
